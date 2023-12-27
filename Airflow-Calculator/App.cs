085679230223333518a1b4b77fa419211978ea08
Md Abdullah <MAbdullah@rensafamily.company>
2023-12-27 16:59:47 +0600

    Add project files.

---
+++ Airflow-Calculator/App.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI;
+using Revit_Sandbox.Commands;
+using System.Reflection;
+
+namespace Revit_Sandbox
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab("BIMLOGIQ");
+            var calculatorPanel = application.CreateRibbonPanel("BIMLOGIQ", "Calculators");
+            calculatorPanel.AddItem(new PushButtonData("CalculateAirflowButton", "Calculate Airflow", Assembly.GetExecutingAssembly().Location, typeof(CalculateAirflowCommand).FullName));
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // Nothing to do for the time being
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Airflow-Calculator/Commands/CalculateAirflowCommand.cs
@@ -0,0 +1,81 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+
+namespace Revit_Sandbox.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CalculateAirflowCommand : IExternalCommand
+    {
+        private double totalAirflow = 0;
+        private List<ElementId> traversedElementIds = new List<ElementId>();
+        private Document doc;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiDocument = commandData.Application.ActiveUIDocument;
+            var document = uiDocument.Document;
+            doc = document;
+            var elementId = uiDocument.Selection.PickObject(ObjectType.Element, "Please select an element to calculate total airflow.");
+            if (elementId == null) return Result.Cancelled;
+
+            var selectedElement = document.GetElement(elementId);
+
+            CalculateTotalAirflow(selectedElement);
+
+            TaskDialog.Show("Airflow", $"AirFlow: {totalAirflow}", TaskDialogCommonButtons.Ok);
+
+            return Result.Succeeded;
+        }
+
+        private static ConnectorSet GetConnectors(Element element)
+        {
+            if (element is FamilyInstance fi && fi.MEPModel != null)
+            {
+                return fi.MEPModel.ConnectorManager?.Connectors;
+            }
+            else if (element is MEPCurve duct)
+            {
+                return duct.ConnectorManager?.Connectors;
+            }
+
+            return null;
+        }
+
+        private void CalculateTotalAirflow(Element element)
+        {
+            totalAirflow = 0;
+            traversedElementIds = new List<ElementId>();
+
+            CalculateAirflow(element);
+        }
+
+        private void CalculateAirflow(Element element)
+        {
+            var airflow = element.get_Parameter(BuiltInParameter.RBS_DUCT_FLOW_PARAM)?.AsDouble() ?? 0;
+            var airflowInLiterPerSecond = UnitUtils.ConvertFromInternalUnits(airflow, UnitTypeId.LitersPerSecond);
+            totalAirflow += Math.Round(airflowInLiterPerSecond);
+
+            traversedElementIds.Add(element.Id);
+
+            ConnectorSet connectorSet = GetConnectors(element);
+            if (connectorSet != null)
+            {
+                foreach (Connector connector in connectorSet)
+                {
+                    foreach (Connector cRef in connector.AllRefs)
+                    {
+                        if (cRef.Owner?.Id != element.Id && cRef.Owner?.UniqueId != null && !traversedElementIds.Contains(cRef.Owner.Id))
+                        {
+                            // Continue calculating for the connected element
+                            CalculateAirflow(doc.GetElement(cRef.Owner.UniqueId));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
